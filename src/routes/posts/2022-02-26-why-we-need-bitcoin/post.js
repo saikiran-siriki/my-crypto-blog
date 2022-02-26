b628bf6b17fd22d92398f895c01c364ffe09f892
saikiran-siriki <sirikisaikiran@gmail.com>
2022-02-26 15:23:00 +0530

    fixed spelling mistakes

--- src/routes/posts/2022-02-26-why-we-need-bitcoin/post.js
+++ src/routes/posts/2022-02-26-why-we-need-bitcoin/post.js
@@ -1,5 +1,7 @@
 export default {
   title: `Why we need Bitcoin?`,
+  ogImage: '/assets/WhyBTC.jpeg',
+  description: 'Importance of BTC going forward!',
   tags: ['Bitcoin'],
   spoiler: "Learn why bitcoin rose in popularity and how it threatens every asset class in existence",
   getContent: () => import('./document.mdx'),
